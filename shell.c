d30208835e3618caf8c9d2a46d6067a2b6fb8201
ananth <ananth.josyula@gmail.com>
2022-11-06 17:43:05 -0600

    v2

--- shell.c
+++ shell.c
@@ -48,23 +48,20 @@ void execute2(char* const args1[], char* const args2[])
         return;
     }
 
-    int prev_pipe, pfds[2];
+    int /*prev_pipe,*/ pfds[2];
 
-    prev_pipe = STDIN_FILENO;
+    //prev_pipe = STDIN_FILENO;
 
     pipe(pfds);
     pid_t pid = fork();
 
-    if (pid == 0) {
-      // Redirect previous pipe to stdin
-      if (prev_pipe != STDIN_FILENO) {
-	dup2(prev_pipe, STDIN_FILENO);
-	close(prev_pipe);
-      }
+    if (pid == 0){
+      close(pfds[0]);
       
       // Redirect stdout to current pipe
       dup2(pfds[1], STDOUT_FILENO);
       close(pfds[1]);
+      
       
       // Start command
       execvp(args1[0], args1);
@@ -72,30 +69,84 @@ void execute2(char* const args1[], char* const args2[])
       perror("execvp failed");
       exit(1);
     }
+    else if (pid > 0){
+      int status = 0;
+      waitpid(pid, &status, 0);
+      if (WEXITSTATUS(status) == 0)
+	printf("*** Child exited sucessfully\n");
+      else
+	printf("*** Child exited with %d\n", WEXITSTATUS(status));
 
-    // Close read end of previous pipe (not needed in the parent)
-    close(prev_pipe);
-    
-    // Close write end of current pipe (not needed in the parent)
-    close(pfds[1]);
-    
-    // Save read end of current pipe to use in next iteration
-    prev_pipe = pfds[0];
+      // Close write end of current pipe (not needed in the parent)
+      close(pfds[1]);
 
-    // Start last command
-    printf("*** Second command:");
-    for (i = 0; args2[i] != NULL; i++)
+      // Start last command
+      printf("*** Second command:");
+      for (i = 0; args2[i] != NULL; i++)
         printf(" %s", args2[i]);
-    printf(" (%d words)\n", i);
+      printf(" (%d words)\n", i);
+      
+      // Get stdin from last pipe
+      dup2(pfds[0], STDIN_FILENO);
+      close(pfds[0]);
+   
+      execvp(args2[0], args2);
     
+      perror("execvp failed");
+      
+      //exit(1);
+    }
+    else{
+      printf("*** Fork failed\n");
+    }
+}
+
+/*void executeN(char* const args[]){
+    size_t i, n;
+    int prev_pipe, pfds[2];
+
+    n = sizeof(commands) / sizeof(*commands);
+    prev_pipe = STDIN_FILENO;
+
+    for (i = 0; i < n - 1; i++) {
+        pipe(pfds);
+
+        if (fork() == 0) {
+            // Redirect previous pipe to stdin
+            if (prev_pipe != STDIN_FILENO) {
+                dup2(prev_pipe, STDIN_FILENO);
+                close(prev_pipe);
+            }
+
+            // Redirect stdout to current pipe
+            dup2(pfds[1], STDOUT_FILENO);
+            close(pfds[1]);
+
+            // Start command
+            execvp(commands[i][0], commands[i]);
+
+            perror("execvp failed");
+            exit(1);
+        }
+
+        // Close read end of previous pipe (not needed in the parent)
+        close(prev_pipe);
+
+        // Close write end of current pipe (not needed in the parent)
+        close(pfds[1]);
+
+        // Save read end of current pipe to use in next iteration
+        prev_pipe = pfds[0];
+    }
+
     // Get stdin from last pipe
     if (prev_pipe != STDIN_FILENO) {
-      dup2(prev_pipe, STDIN_FILENO);
-      close(prev_pipe);
+        dup2(prev_pipe, STDIN_FILENO);
+        close(prev_pipe);
     }
-    
-    execvp(args2[0], args2);
-    
+
+    // Start last command
+    execvp(commands[i][0], commands[i]);
+
     perror("execvp failed");
-    exit(1);   
-}
+    exit(1);*/
